01b186fd125150bd4ebfcfe089561222312edc78
anushakalagi2-alt <anushakalagi2@gmail.com>
2026-01-03 17:37:18 +0530

    Update Taskbar.js

--- Taskbar.js
+++ Taskbar.js
@@ -1,135 +1,117 @@
-import React, {useRef,useState} from 'react'
-import './Task.css';
-import './SIde.css';
+import React, { Component } from 'react'
+import './Task.css'
 
-function Taskbar() {
+export class Task extends Component {
+  constructor(props) {
+    super(props)
   
-    const [todoList, setTodoList] = useState([]);
-    const [filter,setFilter] = useState('all');
-    
-    const inputRef = useRef(null);
-    const priorityRef=useRef(null);
-
-    const add = ()=>{
-            const inputText=inputRef.current.value;
-            const priority =priorityRef.current.value;
-            inputRef.current.value="";
-            if (inputText === ""){
-                return null;
-            }
-            const newTodo={
-                id:Date.now(),
-                text:inputText,
-                priority:priority,
-                isComplete:false,
-                
-            };
-
-            setTodoList((prev) =>[...prev,newTodo]);
-            // inputRef.current.value="";
-
-            const filteredTask = todoList.filter(task => {
-              
-                if (filter === "active") return ! task.completed;
-                if (filter === "done") return task.completed;
-                
-                return true;
-                });
-
-    };
-    const deleteItem=(id)=>{
-      setTodoList(todoList.filter(item => item.id !== id));
+    this.state = {
+      
+       input:'',
+       priority:'low',
+       tasks:[]
     }
+  }
 
-    //  const CheckBox=()=>{
-    //   setTodoList(todoList.filter(t=> t.completed).length);
-    //  }
+  handleSubmit=event=>{
 
-     const ClearAll=()=>{
-      setTodoList(todoList.filter(item=>item === ''));
-     }
-  
-  return (
-    <div className="cont">
-        <div className="main">
-            <div className="box">
-                <input ref={inputRef} type="text"  placeholder="Add a new task..." />
-                <select ref={priorityRef} defaultValue="Medium" id="prioritySelect">
-                    <option  className='low' value='low' >Low</option>
-                    <option className='Medium'  value='medium' >Medium</option>
-                    <option  className='high' value='high' >High</option>
-                </select>
-                <button onClick={add} id="addBtn" >Add Task</button>
-            </div>
-
-            <div className="filters">
-                <button className={filter === 'all' ? "active" : ""} onClick={() =>setFilter("all")} data-filter="all">All ({todoList.length})</button>
-                <button className={filter === 'active' ? "active" : ""} onClick={() =>setFilter("active")} data-filter="active">Active ({todoList.filter(t =>! t.completed).length})</button>
-                <button className={filter === 'done' ? "active" : ""} onClick={() =>setFilter("done")} data-filter="done">Done ({todoList.filter(t=> t.completed).length})</button>
-            </div>
-
-
-            {todoList.map(item => (
-              <>
-              <div className="task-list" key={item.id} >
-             {/* <span> <input type='checkbox'  data-filter="done"></input></span>   */}
-            <span className='task-text' >{item.text}</span>
-            <small className={`priority  ${item.priority}`}> {item.priority}</small>
-            <span className='date'>{new Date().toLocaleDateString()}</span> 
-            <small className='delete-btn' onClick={()=> deleteItem(item.id)}>🗑️</small>
-          </div> 
-         </>
-          
-        ))}
-         
-        {todoList.length === 0 && (
-          
-          <div className="lines">
-             No tasks yet. Add one to get started!
-          </div>
-        )}
-  </div>
-
-
-    {/* sidebar code */}
-
-        <div className='sidebar'>
-        <div className='card'>
-            <div className='title-name'> Statistics</div>
-            <div className='item'>
-                <span>Total</span>
-                <span className='value'>{todoList.length}</span>
-            </div>
-             <div className='item'>
-                <span>Active</span>
-                <span className='value'>{todoList.filter(t =>! t.completed).length}</span>
-            </div>
-             <div className='item'>
-                <span>Completed</span>
-                <span className='value'>{todoList.filter(t=> t.completed).length}</span>
-            </div>
-             <div className='item'>
-                <span>High Priority</span>
-                <span className='value' id='value'>{todoList.filter(t=>t.priority ==="high").length}</span>
-            </div>
-        </div>
-
-        <div className='card1'>
-            <div className='title-name'> Actions</div>
-            <div >
-                <button className='action-buttons'>Clear Completed</button>
-     
-                <button className='btn-danger' onClick={ClearAll}>Clear All</button>
-            </div>
-        </div>
-    </div>
-        
-  </div> 
-  
+     event.preventDefault();
+     const{input,priority}=this.state;
+      const newTask={
+        input:this.state.input,
+        priority,
+        isComplete:false,
+        id:Date.now(),
+        Date:new Date().toLocaleDateString()
+    };
+     this.setState(prevState=>({
+      tasks:[...prevState.tasks,newTask],
+      input:'',
+      priority:'low'
+     }));
     
-)
+  };
+
+  handleTaskChange=(event)=>{
+    this.setState({
+       input:event.target.value
+    })
+  }
+
+  handlePriorityChange=(event)=>{
+    this.setState({
+      priority:event.target.value
+    })
+  }
+
+  deleteTask=(id)=>{
+   this.setState({
+    tasks:this.state.tasks.filter(task=> task.id!== id)
+   })
+    // this.setState({tasks:updatedTasks});
+  };
+   
+  render() {
+
+     const{input,priority}=this.state
+    return (
+      <div className='con'>
+      <header>
+        <h1>✓ Todo Master</h1>
+          <div className='app'>
+            <button className='theme-btn'>Dark</button>
+            <button className='theme-btn'>Light</button>
+            <button className='theme-btn'>Vibrant</button>
+          </div>
+      </header>
+
+      <div className='cont'>
+        <div className='main'>
+          <div className='input' >
+           <form >
+             <input  type='text'  placeholder='Add a new task ?' value={this.state.text} onChange={this.handleTaskChange}></input>
+              <select value={this.state.priority} onChange={this.handlePriorityChange}>
+                <option value='low'>Low</option>
+             <option value='medium'>Medium</option>
+             <option value='high'> High </option>
+              </select>
+              <button onClick={this.handleSubmit} >Add Task</button>
+           </form>
+          </div>
+         
+           <div className="filters">
+                <button className="filter-btn active" data-filter="all">All (<span id="count-all">0</span>)</button>
+                <button className="filter-btn" data-filter="active">Active (<span id="count-active">0</span>)</button>
+                <button className="filter-btn" data-filter="done">Done (<span id="count-done">0</span>)</button>
+            </div>
+                   
+             
+            <div className="taskList" id="taskList">
+              {this.state.tasks.map((task,index)=>(
+                 <div className='task-list' key={index}>
+                 <span className='task-text'>{task.input}</span>
+                 <span className={`priority  ${task.priority}`}>{task.priority}</span>
+                 <span className='date'>{task.Date}</span>
+                 <span className='delete' onClick={()=>this.deleteTask(task.id)}>🗑️</span>
+               </div>
+              ))}
+
+              </div>
+            
+            
+             {/* {this.state.tasks === 0 && (
+              <div className="message">
+                No tasks yet. Add one to get started!
+              </div>
+             )} */}
+              </div>
+    
+      </div>
+  
+   </div> 
+    )      
+  }
 }
-export default Taskbar
 
 
-
+export default Task
